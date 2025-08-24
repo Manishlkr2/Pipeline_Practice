c370f6d3ee930ef260a46f1851460b884ffdc882
Manishlkr2 <manishlkr06@gmail.com>
2025-08-24 17:22:39 +0530

    added Rg & Stg

---
+++ PIPELINE/Main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.41.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "Rg-Manish"
+    storage_account_name = "manishstg1"
+    container_name       = "manishcontainer"
+    key                  = "Manish.tfstate"
+  }
+}
+
+
+provider "azurerm" {
+    features {}
+    subscription_id = "177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
+   
+}
+resource "azurerm_resource_group" "Ganshiv" {
+  name     = "rg-Ganshiv"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "ganshivstg" {
+  name                     = "ganshhivstg1"
+  resource_group_name      = azurerm_resource_group.Ganshiv.name
+  location                 = azurerm_resource_group.Ganshiv.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  
+}
+
